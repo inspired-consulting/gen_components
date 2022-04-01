7f75a40d1c1fca267a97172da260d6dc410177a4
Ruben Wagner <ruben.wagner@globus.ch>
2022-04-01 11:49:03 +0200

    adjust docs

--- README.md
+++ README.md
@@ -7,22 +7,25 @@ It will also generate a component catalogue live view.
 ## Installation
 
 The package can be installed by adding `gen_components` to your list of
-dependencies in `mix.exs`:
+dependencies in `mix.exs`. `runtime: false` should be given, because the
+projet delivers an example endpoint, which should not be started:
 
 ```elixir
 def deps do
   [
-    {:gen_components, "~> 0.1.0"}
+    {:gen_components, "~> 0.1.0", only: [:dev], runtime: false}
   ]
 end
 ```
 
-## Install the ctalogue
+## Install the catalogue
 
 Add the folowing to your router.ex:
 
 ```elixir
-live "/catalogue", ComponentsCatalogueLive, :index
+if Mix.env() == :dev do
+  live "/catalogue", MyAppWeb.ComponentsCatalogueLive, :index
+end
 ```
 
 ## example usage
--- lib/mix/task/gen.components.ex
+++ lib/mix/task/gen.components.ex
@@ -3,28 +3,21 @@ defmodule Mix.Tasks.Gen.Components do
 
   @moduledoc """
   Generates a set of UI components to start a project from.
+
   It will also generate a component catalogue live view.
-
-  ## Install the ctalogue
-
-  Add the folowing to your router.ex:
-
-  ```elixir
-  live "/catalogue", ComponentsCatalogueLive, :index
-  ```
 
   ## example usage
 
   ```bash
-  $ mix gen.components
-  $ mix gen.components -o other_components
+  mix gen.components
+  mix gen.components -o other_components
   ```
 
   ## Options
 
-    * `-o, --output` - lowercase name of the module,
+  * `-o, --output` - lowercase name of the module,
     that will hold the components. Defaults to `components`
-  """
+"""
   use Mix.Task
 
   @default_components_module "components"
@@ -52,34 +45,55 @@ defmodule Mix.Tasks.Gen.Components do
         aliases: [o: :output]
       )
 
+    otp_app = Mix.Phoenix.otp_app()
     components_module = parsed[:output] || @default_components_module
-
-    context_app = Mix.Phoenix.context_app()
-    web_prefix = Mix.Phoenix.web_path(context_app)
     components_binding = Mix.Phoenix.inflect(components_module)
     catalogue_binding = Mix.Phoenix.inflect("components_catalogue")
 
-    catalogue_binding =
-      catalogue_binding
-      |> Keyword.put(:compoents_scoped, components_binding[:scoped])
+    {catalogue_path, components_path, components_scoped, catalogue_scoped, web_module} =
+      if in_umbrella?() do
+        {
+          Mix.Phoenix.context_lib_path(otp_app, "live"),
+          Mix.Phoenix.context_lib_path(otp_app, "#{components_binding[:path]}"),
+          components_binding[:scoped],
+          catalogue_binding[:scoped],
+          catalogue_binding[:base]
+        }
+      else
+        {
+          Mix.Phoenix.web_path(otp_app, "live"),
+          Mix.Phoenix.web_path(otp_app, "#{components_binding[:path]}"),
+          components_binding[:scoped],
+          catalogue_binding[:scoped],
+          catalogue_binding[:web_module]
+        }
+      end
+
+    bindings = [
+      components_scoped: components_scoped,
+      components_module: "#{web_module}.#{components_scoped}",
+      catalogue_scoped: "#{catalogue_scoped}Live",
+      catalogue_module: "#{web_module}.#{catalogue_scoped}Live",
+      web_module: web_module
+    ]
 
     Mix.Phoenix.copy_from(
       paths(),
       @catalogue_template_path,
-      catalogue_binding,
+      bindings,
       Enum.map(
         find_ex(@catalogue_template_file_path),
-        &{:eex, &1, Path.join(web_prefix, "live/#{&1}")}
+        &{:eex, &1, Path.join(catalogue_path, "#{&1}")}
       )
     )
 
     Mix.Phoenix.copy_from(
       paths(),
       @components_template_path,
-      components_binding,
+      bindings,
       Enum.map(
         find_ex(@components_template_file_path),
-        &{:eex, &1, Path.join(web_prefix, "#{components_binding[:path]}/#{&1}")}
+        &{:eex, &1, Path.join(components_path, "#{&1}")}
       )
     )
   end
@@ -92,5 +106,12 @@ defmodule Mix.Tasks.Gen.Components do
     "#{folder}/**/*.ex"
     |> Path.wildcard()
     |> Enum.map(&Path.relative_to(&1, "#{folder}"))
+  end
+
+  defp in_umbrella?() do
+    case Mix.Project.config()[:config_path] do
+      "../../" <> _ -> true
+      _ -> false
+    end
   end
 end
