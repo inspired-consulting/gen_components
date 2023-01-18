3e6b9cbfeb11a6fb42d06c4cb912694f15dd0c0e
Alptug Dingil <alptug.dingil@inspired.consulting>
2023-01-18 14:33:42 +0100

    added mdi icon per cdn

--- components_demo/assets/js/app.js
+++ components_demo/assets/js/app.js
@@ -14,6 +14,7 @@ let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfTo
 // connect if there are any LiveViews on the page
 liveSocket.connect()
 
+
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
